dde39d24c3b306b9326413b93cf07e54802d7b86
Erick White <erickwhitebusiness@gmail.com>
2022-06-17 23:04:13 -0600

    Minor menu changes
    +Added transition time for menu blur effect

--- scripts/menuHandler.js
+++ scripts/menuHandler.js
@@ -2,16 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let menuButton = document.getElementById("menu-button")
     let menu = document.getElementById("menu");
+    let main = document.querySelector("main");
     let menuClosed = menu.style.display === "none";
 
     menuButton.onclick = () => {
         if (menuClosed) {
             menu.style.display = "none";
-            document.querySelector("main").style.filter = "blur(0px)";
+            main.style.filter = "blur(0px)";
             menuClosed = false;
         } else {
             menu.style.display = "block";
-            document.querySelector("main").style.filter = "blur(10px)";
+            main.style.filter = "blur(10px)";
             menuClosed = true;
         }
     };
