9f15003d953b07f0f38b6030b7c47c4353121df8
Erick White <erickwhitebusiness@gmail.com>
2023-01-03 18:38:07 -0700

    Added Relevant Courses page and content
    + Added Relevant Courses page and content

--- components/page-menu.js
+++ components/page-menu.js
@@ -15,20 +15,21 @@ let pageMenu = `
                 <li class="nav-item"><a href="index.html">Home</a></li>
                 <li class="nav-item"><a href="about-me.html">About Me</a></li>
                 <li class="nav-item"><a href="philosophy-of-education.html">Philosophy of Education</a></li>
+                <li class="nav-item dropdown"><a>What I Do ▼</a></li>
+                <ul class="nav-dropdown">
+                    <li class="nav-item-dropdown"><a href="relevant-courses.html">Relevant Courses</a></li>
+                </ul>
                 <li class="nav-item resume"><a href="resume.html">Résumé</a></li>
             </ul>
         </nav>
 `;
 
-// TODO: Fill out these pages and add this between About Me and Résumé
+// TODO: Fill out these pages and add them to the nav dropdown submenu
 /*
-                <li class="nav-item dropdown"><a>What I Do ▼</a></li>
-                <ul class="nav-dropdown">
-                    <li class="nav-item-dropdown"><a href="projects.html">Projects</a></li>
-                    <li class="nav-item-dropdown"><a href="creative-work.html">Creative Work</a></li>
-                    <li class="nav-item-dropdown"><a href="writing.html">Writing</a></li>
-                    <li class="nav-item-dropdown"><a href="work-and-experience.html">Work and Experience</a></li>
-                </ul>
+                <li class="nav-item-dropdown"><a href="projects.html">Projects</a></li>
+                <li class="nav-item-dropdown"><a href="creative-work.html">Creative Work</a></li>
+                <li class="nav-item-dropdown"><a href="writing.html">Writing</a></li>
+                <li class="nav-item-dropdown"><a href="work-and-experience.html">Work and Experience</a></li>
  */
 
 document.getElementById("page-menu").innerHTML = pageMenu;
