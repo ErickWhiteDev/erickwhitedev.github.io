f5b1befccb4499d514d6265cf3e1cc543759664a
Erick White <erickwhitebusiness@gmail.com>
2023-03-04 12:52:11 -0700

    Updated styling
    + Added bar to top of page for menu items on scroll
    + Added home page media query for 4K screens
    
    * Changed Philosophy of Education page styling

--- components/page-menu.js
+++ components/page-menu.js
@@ -9,20 +9,20 @@ let pageMenu = `
             <use href="assets/svg/theme.svg#theme"/>
         </svg>
     </button>
-        <nav id="menu">
-            <div class="spacer"></div>
-            <ul>
-                <li class="nav-item"><a href="index.html">Home</a></li>
-                <li class="nav-item"><a href="about-me.html">About Me</a></li>
-                <li class="nav-item"><a href="philosophy-of-education.html">Philosophy of Education</a></li>
-                <li class="nav-item dropdown"><a>What I Do ▼</a></li>
-                <ul class="nav-dropdown">
-                    <li class="nav-item-dropdown"><a href="projects.html">Projects</a></li>
-                    <li class="nav-item-dropdown"><a href="relevant-courses.html">Relevant Courses</a></li>
-                </ul>
-                <li class="nav-item resume"><a href="resume.html">Résumé</a></li>
+    <nav id="menu">
+        <div class="spacer"></div>
+        <ul>
+            <li class="nav-item"><a href="index.html">Home</a></li>
+            <li class="nav-item"><a href="about-me.html">About Me</a></li>
+            <li class="nav-item"><a href="philosophy-of-education.html">Philosophy of Education</a></li>
+            <li class="nav-item dropdown"><a>What I Do ▼</a></li>
+            <ul class="nav-dropdown">
+                <li class="nav-item-dropdown"><a href="projects.html">Projects</a></li>
+                <li class="nav-item-dropdown"><a href="relevant-courses.html">Relevant Courses</a></li>
             </ul>
-        </nav>
+            <li class="nav-item resume"><a href="resume.html">Résumé</a></li>
+        </ul>
+    </nav>
 `;
 
 // TODO: Fill out these pages and add them to the nav dropdown submenu
