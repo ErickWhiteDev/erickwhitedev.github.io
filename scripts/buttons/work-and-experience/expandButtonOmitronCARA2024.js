057262a720725e153578f5b5eca70ad374f0c963
Erick White <erickwhitebusiness@gmail.com>
2024-10-02 23:46:32 -0600

    Added CARA 2024 summer internship information and replaced headshot with smaller file to improve loading times

---
+++ scripts/buttons/work-and-experience/expandButtonOmitronCARA2024.js
@@ -0,0 +1,8 @@
+let expandOmitronCARA2024 = document.getElementById("expand-omitron-cara-2024");
+
+expandOmitronCARA2024.onclick = () => {
+    expandOmitronCARA2024.previousElementSibling.style.marginBottom = "0";
+    expandOmitronCARA2024.nextElementSibling.style.display = "block";
+    expandOmitronCARA2024.style.display = "none";
+    expandOmitronCARA2024.style.display = "none";
+};
