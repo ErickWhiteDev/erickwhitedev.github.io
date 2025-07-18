56f1b509480f8234ebba4709ae81c7027edcfa87
Erick White <erickwhitebusiness@gmail.com>
2025-07-18 15:56:37 -0600

    + Added APPM 4600, APPM 4610, and MCM 2025 to writing portfolio + Added LAIR and CSML experience
    * Update home page
    * Updated résumé
    
    - (Possibly) temporarily removed relevant courses page

---
+++ scripts/buttons/work-and-experience/expandButtonLAIR2024.js
@@ -0,0 +1,8 @@
+let expandLAIR2024 = document.getElementById("expand-lair-2024");
+
+expandLAIR2024.onclick = () => {
+    expandLAIR2024.previousElementSibling.style.marginBottom = "0";
+    expandLAIR2024.nextElementSibling.style.display = "block";
+    expandLAIR2024.style.display = "none";
+    expandLAIR2024.style.display = "none";
+};
